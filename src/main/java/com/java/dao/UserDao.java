df93b5ad08ce4d34c1154a2636066445d9caf45f
xuhui1208 <290264810@qq.com>
2018-08-04 15:09:09 +0800

    ssm 测试2

--- src/main/java/com/java/dao/UserDao.java
+++ src/main/java/com/java/dao/UserDao.java
@@ -2,7 +2,12 @@ package com.java.dao;
 
 import com.java.entity.User;
 
+import java.util.List;
+
 
 public interface UserDao {
-    public User queryAll(String id);
+
+    public User queryUserById(String id);
+
+    public List<User> queryAll();
 }
