46b6f15f3b1a4d11bac283a7ea96350a5d7ea0b2
xuhui1208 <290264810@qq.com>
2018-08-04 14:38:53 +0800

    ssm 测试2

--- src/main/java/com/java/demo/Test2.java
+++ src/main/java/com/java/demo/Test2.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Date;
+
 /**
  * @atuhor Tiance
  * @date 2018/8/3 22:06
@@ -15,7 +17,9 @@ public class Test2 {
     public void test1() {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao dao = (UserDao) ctx.getBean("userDao");
+        System.out.println(new Date()+"     "+"1");
         User user = dao.queryAll("1");
+        System.out.println(new Date()+"      "+"2");
         System.err.println(user);
     }
 
